b57452bf14df816cec021bf2749311e2e15273ef
Ivan Urra <ivan.urra.wd@gmail.com>
2024-05-01 08:21:19 +0200

    feat: api config

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,11 @@
+import { API_URL } from "./config";
+
+async function getGames() {
+  const res = fetch(
+    `${API_URL}/videogames?populate[platforms][fields][0]=name&populate[cover][fields][0]=url`
+  );
+}
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
