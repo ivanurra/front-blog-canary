544a20eb7fa2d84f6289fc8d0281944de7abd2b1
Ivan Urra <ivan.urra.wd@gmail.com>
2024-05-05 11:46:28 +0200

    feat: images

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { API_URL } from "./config";
+import { API_URL, STRAPI_URL } from "./config";
 
 async function getGames() {
   const res = await fetch(
@@ -12,40 +12,37 @@ async function getGames() {
   return data;
 }
 
-export default function Home({ games }) {
+function getCoverImage({ attributes }) {
+  const { url } = attributes.cover.data.attributes;
+  return `${STRAPI_URL}${url}`;
+}
+
+export default async function Home() {
+  const games = await getGames();
+  console.log(games);
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
+    <main class="flex min-h-screen flex-col items-left p-24">
       {games.map(({ attributes, id }) => (
         <Link
           key={id}
           href="#"
+          class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
         >
-          <a className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-            <img
-              className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
-              src={attributes.cover.data.attributes.url}
-              alt={attributes.title}
-            />
-            <div className="flex flex-col justify-between p-4 leading-normal">
-              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                {attributes.title}
-              </h5>
-              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                {attributes.description}
-              </p>
-            </div>
-          </a>
+          <img
+            class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
+            src={getCoverImage({ attributes })}
+            alt={attributes.title}
+          />
+          <div class="flex flex-col justify-between p-4 leading-normal">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              {attributes.title}
+            </h5>
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+               {attributes.largeText}
+            </p>
+          </div>
         </Link>
       ))}
     </main>
   );
-}
-
-export async function getServerSideProps() {
-  const games = await getGames();
-  return {
-    props: {
-      games,
-    },
-  };
 }
