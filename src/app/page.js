442e820a17b28978433b088fd20bbaf848e7e74c
Ivan Urra <ivan.urra.wd@gmail.com>
2024-05-09 08:09:55 +0200

    fix: className

--- src/app/page.js
+++ src/app/page.js
@@ -21,7 +21,7 @@ export default async function Home() {
   const games = await getGames();
   console.log(games);
   return (
-    <main class="flex min-h-screen flex-col items-left p-24">
+    <main className="flex min-h-screen flex-col items-left p-24">
       {games.map(({ attributes, id }) => (
         <Link
           key={id}
@@ -29,15 +29,15 @@ export default async function Home() {
           class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
         >
           <img
-            class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
+            className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
             src={getCoverImage({ attributes })}
             alt={attributes.title}
           />
-          <div class="flex flex-col justify-between p-4 leading-normal">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          <div className="flex flex-col justify-between p-4 leading-normal">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
               {attributes.title}
             </h5>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
                {attributes.largeText}
             </p>
           </div>
